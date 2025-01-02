2cd12f8cae65c8e3d8dc0142d1ba3b931b938e60
AkiraShingu <shinguakira1022@gmail.com>
2025-01-03 01:38:52 +0900

    Add Sidebar component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,13 @@ import React from 'react'
 import HeaderBox from '@/components/ui/HeaderBox';
 import { getUserInfo ,getLoggedInUser} from '@/lib/actions/user.actions';
 import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
+import RightSidebar from '@/components/ui/RightSidebar';
 
 export const Home = async( ) => {
   // const currentPage = Number(page as string) || 1;// set first page as default
-  const loggedIn = {firstName: "Adrian"};
+  const loggedIn = {firstName: "Adrian", 
+    lastName:"JSM",
+    email:"contact@akirashingu.com"};
   // const accounts = await getAccountTypeColors({
   //   userId: loggedIn.$id
   // });
@@ -25,7 +28,13 @@ export const Home = async( ) => {
               totalCurrentBalance={0}
             />
         </header>
+
+        RECENT Transations
       </div>
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 123.50},{currentBalance: 123.50}]}/>
     </section>
   )
 }
